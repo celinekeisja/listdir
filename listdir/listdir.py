3b9aba39e719cd04a8be8a8ddd5c8fcd9e454b7a
celinekeisja <celinekeisja@gmail.com>
2019-10-04 17:12:24 +0800

    Added listdir project

---
+++ listdir/listdir.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+import csv
+import glob
+
+
+def directory_files(name, files):
+    # Puts the Parent Path, File Name, and File Size of files within the specified directory in a CSV file.
+    with open(f'{name}.csv', 'w+') as csv_file:
+        field_n = ['Parent Path', 'File Name', 'File Size']
+        writer = csv.DictWriter(csv_file, fieldnames=field_n)
+        writer.writeheader()
+        for r, d, f in os.walk(directory_name):
+            for file in f:
+                d = {"Parent Path": r, "File Name": file, "File Size": os.path.getsize(r+"//"+file)}
+                writer.writerow(d)
+    return f'Created {name}.csv!'
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument("name")
+    parser.add_argument('-d', '--directory_files', action='store_true', help='Stores the name of a directory.')
+    args = parser.parse_args()
+    if args.directory_files:
+        directory_name = args.path
+        output_name = args.name
+        directory_files(output_name, directory_name)
