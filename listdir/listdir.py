ecd4c0331c7258fec116b44bae3c1d84c93ef0da
celinekeisja <celinekeisja@gmail.com>
2019-10-14 15:54:43 +0800

    Updated listdir.py

--- listdir/listdir.py
+++ listdir/listdir.py
@@ -1,18 +1,45 @@
 import os
 import argparse
 import csv
+import hashlib
+import zipfile
+
+
+def hash_file(file, algorithm):
+    # Hashes the file using MD5 algorithm.
+    if algorithm == 'md5':
+        hasher = hashlib.md5()
+    elif algorithm == 'sha1':
+        hasher = hashlib.sha1()
+    blocksize = 65536
+    with open(file, 'rb') as f:
+        buf = f.read(blocksize)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = f.read(blocksize)
+
+    return hasher.hexdigest()
+
+
+def zip_file(name):
+    # Zips the file under the user-defined name.
+    with zipfile.ZipFile(f"{name}.zip", 'w') as file:
+        file.write(name)
 
 
 def directory_files(name, files):
     # Puts the Parent Path, File Name, and File Size of files within the specified directory in a CSV file.
     with open(f'{name}.csv', 'w+', newline='') as csv_file:
-        field_n = ['Parent Path', 'File Name', 'File Size']
+        field_n = ['Parent Path', 'File Name', 'File Size', 'MD5', 'SHA1']
         writer = csv.DictWriter(csv_file, fieldnames=field_n)
         writer.writeheader()
-        for r, d, f in os.walk(directory_name):
+        for r, d, f in os.walk(files):
             for file in f:
-                d = {"Parent Path": r, "File Name": file, "File Size": os.path.getsize(r+"//"+file)}
+                size = os.path.getsize(r+"//"+file)
+                d = {"Parent Path": r, "File Name": file, "File Size": size,
+                     "MD5": hash_file(f"{r}/{file}", 'md5'), "SHA1": hash_file(f"{r}/{file}", 'sha1')}
                 writer.writerow(d)
+    zip_file(f'{name}.csv')
     return f'Created {name}.csv!'
 
 
@@ -20,9 +47,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("path")
     parser.add_argument("name")
-    parser.add_argument('-d', '--directory_files', action='store_true', help='Stores the name of a directory.')
     args = parser.parse_args()
-    if args.directory_files:
+    if args.path:
         directory_name = args.path
         output_name = args.name
         directory_files(output_name, directory_name)
