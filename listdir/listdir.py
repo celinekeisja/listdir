fc0e8a1c1671460cabf79bdc44c6a586deb65926
Celine Keisja <celinekeisja@gmail.com>
2019-10-21 17:40:18 +0800

    Updates

--- listdir/listdir.py
+++ listdir/listdir.py
@@ -35,55 +35,72 @@ def hash_file(file, algorithm):
     elif algorithm == 'sha1':
         hasher = hashlib.sha1()
     blocksize = 65536
-    with open(file, 'rb') as f:
-        buf = f.read(blocksize)
-        while len(buf) > 0:
-            hasher.update(buf)
+    try:
+        with open(file, 'rb') as f:
             buf = f.read(blocksize)
-    result = hasher.hexdigest()
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = f.read(blocksize)
+        result = hasher.hexdigest()
+    except:
+        logger.error('Unable to Hash.')
     return result
 
 
 def zip_file(name):
     """ Zip the file under the user-defined name. """
-    logging.info(f'Zipping {name}')
-    with zipfile.ZipFile(f"{name}.zip", 'w', zipfile.ZIP_DEFLATED) as file:
-        logging.info('Creating zip...')
-        file.write(name)
-    logging.info('Zipping done.')
+    logger.info(f'Zipping {name}')
+    try:
+        with zipfile.ZipFile(f"{name}.zip", 'w', zipfile.ZIP_DEFLATED) as file:
+            logger.info('Creating zip...')
+            file.write(name)
+    except:
+        logger.error('An error occurred while zipping.')
+    logger.info('Zipping done.')
     return f'{name}.zip'
 
 
 def datetime_filename(given_name):
     """ Include the current date and time onto the specified file name. """
-    logging.info(f'Naming {given_name}')
-    final_name = f'{given_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
-    logging.info(f'Result: {final_name}')
+    logger.info(f'Naming {given_name}')
+    try:
+        final_name = f'{given_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+    except:
+        logger.error('Unable to name properly.')
+    logger.info(f'Result: {final_name}')
     return final_name
 
 
-def directory_files(name, files):
+def csv_files(name, files):
     """ Put the Parent Path, File Name, and File Size of files within the specified directory in a CSV file. """
-    logging.info('Opened new .csv file.')
-    with open(f'{name}.csv', 'w+', newline='') as csv_file:
-        field_n = ['Parent Path', 'File Name', 'File Size', 'MD5', 'SHA1']
-        writer = csv.DictWriter(csv_file, fieldnames=field_n)
-        logging.info(f'Included the ff. headers: {field_n}.')
-        writer.writeheader()
-        logging.info('Writing rows...')
-        for r, d, f in os.walk(files):
-            for file in f:
-                size = os.path.getsize(r+"//"+file)
-                d = {"Parent Path": r, "File Name": file, "File Size": size,
-                     "MD5": hash_file(f"{r}/{file}", 'md5'), "SHA1": hash_file(f"{r}/{file}", 'sha1')}
-                writer.writerow(d)
-    logging.info('Finished with .csv file. Onto zipping...')
-    zip_file(f'{name}.csv')
-    logging.info(f'Finished creating {name}.csv.')
+    logger.info('Opening new .csv file.')
+    try:
+        with open(f'{name}.csv', 'w+', newline='') as csv_file:
+            field_n = ['Parent Path', 'File Name', 'File Size', 'MD5', 'SHA1']
+            writer = csv.DictWriter(csv_file, fieldnames=field_n)
+            logger.info(f'Including the ff. headers: {field_n}.')
+            try:
+                writer.writeheader()
+            except:
+                logger.error('Unable to write header.')
+            logger.info('Writing rows...')
+            try:
+                for r, d, f in os.walk(files):
+                    for file in f:
+                        size = os.path.getsize(r+os.sep+file)
+                        d = {"Parent Path": r, "File Name": file, "File Size": size,
+                             "MD5": hash_file(f"{r}{os.sep}{file}", 'md5'), "SHA1": hash_file(f"{r}{os.sep}{file}", 'sha1')}
+                        writer.writerow(d)
+            except:
+                logger.error("Unable to write file.")
+    except:
+        logger.error('Unable to write a new file.')
+    logger.info('Finished with .csv file. Onto zipping...')
     return f'{name}.csv'
 
 
 def main():
+    """ Main function """
     config = configparser.ConfigParser()
     o = os.path.dirname(__file__)
     config.read(o + 'config.ini')
@@ -93,10 +110,15 @@ def main():
     parser.add_argument("name", nargs="?", default=config['default']['output_name'])
     args = parser.parse_args()
     directory_name = args.path
-    logging.info(f'\nNew Log')
     output_name = datetime_filename(args.name)
-    directory_files(output_name, directory_name)
+
+    try:
+        new_name = zip_file(csv_files(output_name, directory_name))
+    except:
+        logger.error('Unable to zip file.')
+    logger.info(f'Finished creating {new_name}.')
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
     main()
--- tests/test_listdir.py
+++ tests/test_listdir.py
@@ -18,6 +18,6 @@ def test_sha1():
                              'sha1') == '27d1a529469ce7cfca656744461aaf3679015953'
 
 
-def test_dir():
+def test_csv():
     assert listdir.directory_files(test_filename, r'C:\Users\TEU_USER\PycharmProjects\listdir\tests\test.txt') == 'test_file.csv'
     assert os.path.isfile(r'C:\Users\TEU_USER\PycharmProjects\listdir\tests\test_file.csv')
