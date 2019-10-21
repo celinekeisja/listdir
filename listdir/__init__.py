bf4b5f57d5a3d79ed6eebf1a9e8717e070db1274
Celine Keisja <celinekeisja@gmail.com>
2019-10-21 17:07:23 +0800

    Added files for .json output

--- listdir/__init__.py
+++ listdir/__init__.py
@@ -1,1 +1,5 @@
+from listdir.listdir import *
+from listdir.listdir import setup_logging
+
 name = 'listdir'
+setup_logging(default_path=os.path.join("/".join(__file__.split('/')[:-1]), 'config', 'loggingConfig.yaml'))
--- listdir/listdir.py
+++ listdir/listdir.py
@@ -7,6 +7,7 @@ import configparser
 from datetime import datetime
 import logging.config
 import yaml
+import json
 
 
 def setup_logging(
@@ -28,6 +29,35 @@ def setup_logging(
     return 'Setting up logging.'
 
 
+def json_files(name, files):
+    """ Put the Parent Path, File Name, and File Size of files within the specified directory in a JSON file. """
+    logger.info('Opening new .json file.')
+    try:
+        with open(f'{name}.json', 'w+', newline='') as json_file:
+            logger.info('Writing rows...')
+            try:
+                for r, d, f in os.walk(files):
+                    for file in f:
+                        size = os.path.getsize(r+os.sep+file)
+                        path_to_hash = f"{r}{os.sep}{file}"
+                        data = {}
+                        data[file] = []
+                        data[file].append({
+                            'Parent Path': r,
+                            'File Name': file,
+                            'File Size': size,
+                            'MD5': hash_file(path_to_hash, 'md5'),
+                            'SHA1': hash_file(path_to_hash, 'sha1')
+                        })
+                        json.dump(data, json_file, indent=2)
+            except:
+                logger.error("Unable to write file.")
+    except:
+        logger.error('Unable to write a new file.')
+    logger.info('Finished with .csv file. Onto zipping...')
+    return f'{name}.json'
+
+
 def hash_file(file, algorithm):
     """ Hash the file using MD5 algorithm. """
     if algorithm == 'md5':
@@ -35,55 +65,72 @@ def hash_file(file, algorithm):
     elif algorithm == 'sha1':
         hasher = hashlib.sha1()
     blocksize = 65536
-    with open(file, 'rb') as f:
-        buf = f.read(blocksize)
-        while len(buf) > 0:
-            hasher.update(buf)
+    try:
+        with open(file, 'rb') as f:
             buf = f.read(blocksize)
-    result = hasher.hexdigest()
+            while len(buf) > 0:
+                hasher.update(buf)
+                buf = f.read(blocksize)
+        result = hasher.hexdigest()
+    except:
+        logger.error('Unable to Hash.')
     return result
 
 
 def zip_file(name):
     """ Zip the file under the user-defined name. """
-    logging.info(f'Zipping {name}')
-    with zipfile.ZipFile(f"{name}.zip", 'w', zipfile.ZIP_DEFLATED) as file:
-        logging.info('Creating zip...')
-        file.write(name)
-    logging.info('Zipping done.')
+    logger.info(f'Zipping {name}')
+    try:
+        with zipfile.ZipFile(f"{name}.zip", 'w', zipfile.ZIP_DEFLATED) as file:
+            logger.info('Creating zip...')
+            file.write(name)
+    except:
+        logger.error('An error occurred while zipping.')
+    logger.info('Zipping done.')
     return f'{name}.zip'
 
 
 def datetime_filename(given_name):
     """ Include the current date and time onto the specified file name. """
-    logging.info(f'Naming {given_name}')
-    final_name = f'{given_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
-    logging.info(f'Result: {final_name}')
+    logger.info(f'Naming {given_name}')
+    try:
+        final_name = f'{given_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+    except:
+        logger.error('Unable to name properly.')
+    logger.info(f'Result: {final_name}')
     return final_name
 
 
-def directory_files(name, files):
+def csv_files(name, files):
     """ Put the Parent Path, File Name, and File Size of files within the specified directory in a CSV file. """
-    logging.info('Opened new .csv file.')
-    with open(f'{name}.csv', 'w+', newline='') as csv_file:
-        field_n = ['Parent Path', 'File Name', 'File Size', 'MD5', 'SHA1']
-        writer = csv.DictWriter(csv_file, fieldnames=field_n)
-        logging.info(f'Included the ff. headers: {field_n}.')
-        writer.writeheader()
-        logging.info('Writing rows...')
-        for r, d, f in os.walk(files):
-            for file in f:
-                size = os.path.getsize(r+"//"+file)
-                d = {"Parent Path": r, "File Name": file, "File Size": size,
-                     "MD5": hash_file(f"{r}/{file}", 'md5'), "SHA1": hash_file(f"{r}/{file}", 'sha1')}
-                writer.writerow(d)
-    logging.info('Finished with .csv file. Onto zipping...')
-    zip_file(f'{name}.csv')
-    logging.info(f'Finished creating {name}.csv.')
+    logger.info('Opening new .csv file.')
+    try:
+        with open(f'{name}.csv', 'w+', newline='') as csv_file:
+            field_n = ['Parent Path', 'File Name', 'File Size', 'MD5', 'SHA1']
+            writer = csv.DictWriter(csv_file, fieldnames=field_n)
+            logger.info(f'Including the ff. headers: {field_n}.')
+            try:
+                writer.writeheader()
+            except:
+                logger.error('Unable to write header.')
+            logger.info('Writing rows...')
+            try:
+                for r, d, f in os.walk(files):
+                    for file in f:
+                        size = os.path.getsize(r+os.sep+file)
+                        d = {"Parent Path": r, "File Name": file, "File Size": size,
+                             "MD5": hash_file(f"{r}{os.sep}{file}", 'md5'), "SHA1": hash_file(f"{r}{os.sep}{file}", 'sha1')}
+                        writer.writerow(d)
+            except:
+                logger.error("Unable to write file.")
+    except:
+        logger.error('Unable to write a new file.')
+    logger.info('Finished with .csv file. Onto zipping...')
     return f'{name}.csv'
 
 
 def main():
+    """ Main function """
     config = configparser.ConfigParser()
     o = os.path.dirname(__file__)
     config.read(o + 'config.ini')
@@ -91,12 +138,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("path", nargs="?", default=config['default']['input_path'])
     parser.add_argument("name", nargs="?", default=config['default']['output_name'])
+    parser.add_argument("-j", "--json", action="store_true")
     args = parser.parse_args()
     directory_name = args.path
-    logging.info(f'\nNew Log')
     output_name = datetime_filename(args.name)
-    directory_files(output_name, directory_name)
+
+    if args.json:
+        try:
+            new_name = zip_file(json_files(output_name, directory_name))
+        except:
+            logger.error('Unable to zip file.')
+    else:
+        try:
+            new_name = zip_file(csv_files(output_name, directory_name))
+        except:
+            logger.error('Unable to zip file.')
+    logger.info(f'Finished creating {new_name}.')
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
     main()
